ee57a289043157d07531f7cb689f747c9c8c9e0c
juan <juangrisales.c@gmail.com>
2021-05-20 21:49:50 -0500

    Base application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewPostComponent } from './components/posts/new-post/new-post.component';
+import { NewPostModule } from './components/posts/new-post/new-post.module';
+import { PostComponent } from './components/posts/post/post.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewPostComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NewPostModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
